ed062c3e048615fab6657855f0f6fba8a509c047
EC2 Default User <ec2-user@ip-172-31-84-2.ec2.internal>
2021-06-11 04:38:13 +0000

    {start}3-5

--- db/migrate/20210610210151_remove_bod_from_lists.rb
+++
@@ -1,5 +0,0 @@
-class RemoveBodFromLists < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :lists, :bod, :body
-  end
-end
--- db/migrate/cd.rb
+++
@@ -1,5 +0,0 @@
-class RemovebodFromlists < ActiveRecord::Migration[5.2]
-  def change
-    remname_clumn :lists, :bod, :body
-  end
-end
